796314bac1324f53360375ce9ce0d6e149b30564
jaspriya <jaswanthi.priya@sapientrazorfish.com>
2018-05-17 15:43:07 -0500

    mallory fonts-added

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const webpack = require("./webpack.base.config");
 
 module.exports = {
-  require: [path.join(__dirname, "./styles/normalize.css")],
+  require: [
+    path.join(__dirname, "./styles/normalize.css"),
+    path.join(__dirname, "./styles/fonts/font-mallory.css")
+  ],
   sections: [
     {
       name: "Atomic Components",
@@ -18,5 +21,6 @@ module.exports = {
   },
   styleguideComponents: {
     Wrapper: path.join(__dirname, "/lib/Themewrapper.js")
-  }
+  },
+  assetsDir: path.join(__dirname, "/assets")
 };
