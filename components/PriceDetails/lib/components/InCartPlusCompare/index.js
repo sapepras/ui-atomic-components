22d8f87523328405826ed9d69383db2f41b9135e
Prashanth Reddy <somidi.PrashanthReddy@academy.com>
2018-07-13 16:11:01 +0530

    compare at price text color is changed to #333333

--- components/PriceDetails/lib/components/InCartPlusCompare/index.js
+++ components/PriceDetails/lib/components/InCartPlusCompare/index.js
@@ -18,7 +18,7 @@ const InCartPlusCompare = ({ listPrice }) => {
       <div className="c-price-in-cart" style={{ color: '#ee0000' }}>
         Our Price in Cart
       </div>
-      <div className="c-price-compare" style={{ color: '#767676' }}>
+      <div className="c-price-compare" style={{ color: '#333333' }}>
         Compare at {formattedListPrice}
       </div>
     </div>
