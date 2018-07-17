fe7e68442759d67c6cf2497430bb11285bb1e296
Tejas Upmanyu <tejasupmanyu@gmail.com>
2018-07-17 19:01:03 +0530

    Bug Fixes and Improvements

--- components/Dropdown/index.js
+++ components/Dropdown/index.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { css } from 'react-emotion';
 
 const DropdownStyle = props => css`
-    position: absolute;
+    position: relative;
     ul {
         margin: 0;
         padding: 0;
         width: 100%;
         list-style-type: none;
+        position: absolute;
         background: #fff;
         z-index: 1;
         border-radius: ${props.listBorderRadius ? props.listBorderRadius : '5px'};
@@ -19,8 +20,16 @@ const DropdownStyle = props => css`
         font-weight: normal;
         cursor: pointer;
         &:hover {
-            color: #fff;
             background: #0055a6;
+            span {
+                color: #fff;
+            }
+        }
+        &.active {
+            background: #0055a6;
+            span {
+                color: #fff;
+            }
         }
         &:first-child {
             &:hover {
@@ -39,7 +48,7 @@ const DropdownStyle = props => css`
 const btnStyle = props => css`
     display: flex;
     padding: 0.5rem 1rem;
-    width: ${props.width ? props.width : '30rem'};
+    width: ${props.width ? props.width : '100%'};
     height: ${props.height ? props.height : '3.5rem'};
     line-height: 1.25;
     background-color: #fff;
@@ -52,40 +61,49 @@ const listStyle = props => css`
     width: ${props.width}
 `;
 
-const titleClassWrapper = props => css`
-    ${props.titleClass};
-    color: #fff;
-`;
-
-const subtitleClassWrapper = props => css`
-    ${props.subtitleClass};
-    color: #fff;
-`;
-
 class Dropdown extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dropdowncollapse: false,
+            activeListItem: this.props.initiallySelectedOption < this.props.DropdownOptions.length ? this.props.initiallySelectedOption : 0,
             selectedOption: this.props.initiallySelectedOption < this.props.DropdownOptions.length ? this.props.DropdownOptions[this.props.initiallySelectedOption] : { title: 'Select' }
         };
         this.onSelectWrapper = this.onSelectWrapper.bind(this);
         this.renderButtonContents = this.renderButtonContents.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
     }
 
     onSelectWrapper(value, onSelect, index) {
-        this.setState({ selectedOption: value, dropdowncollapse: false });
+        this.setState({ selectedOption: value, dropdowncollapse: false, activeListItem: index });
         onSelect(index);
     }
+    setWrapperRef(node) {
+        this.wrapperRef = node;
+    }
+    handleClickOutside(event) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            this.setState({ dropdowncollapse: !this.state.dropdowncollapse });
+        }
+    }
 
-    renderButtonContents(item, titleClass, subtitleClass) {
+    renderButtonContents(item, titleClass = '', subtitleClass = '') {
         if (typeof item === 'object') {
             const content = (
-              <div className="d-flex flex-column">
-                <div className={`d-flex justify-content-start ${titleClassWrapper(titleClass)} `}>
+              <div>
+                <div className={`d-flex justify-content-start ${titleClass} `}>
                   {item.title}
                 </div>
-                <div className={`d-flex justify-content-start ${subtitleClassWrapper(subtitleClass)} `}>
+                <div className={`d-flex justify-content-start ${subtitleClass} `}>
                   {item.subtitle}
                 </div>
               </div>);
@@ -99,14 +117,14 @@ class Dropdown extends Component {
             DropdownOptions, multi, titleClass, subtitleClass, onSelectOption, disabled
         } = this.props;
         return (
-          <div className={`${DropdownStyle(this.props)}`}>
+          <div ref={this.setWrapperRef} className={`${DropdownStyle(this.props)}`}>
             <button type="button" className={`${btnStyle(this.props)} d-flex justify-content-between align-items-center`} disabled={disabled} onClick={() => this.setState({ dropdowncollapse: !this.state.dropdowncollapse })}>
               {this.renderButtonContents(this.state.selectedOption, titleClass, subtitleClass)}
               <span className={!this.state.dropdowncollapse ? 'academyicon icon-chevron-down' : 'academyicon icon-chevron-up'} />
             </button>
             {this.state.dropdowncollapse && (
-            <ul className={`${listStyle(this.props)} align-items-center`} role="presentation">
-              <DropdownList multi={multi} titleClass={titleClass} subtitleClass={subtitleClass} options={DropdownOptions} onSelect={(value, index) => this.onSelectWrapper(value, onSelectOption, index)} />
+            <ul className={`${listStyle(this.props)} align-items-center`} role="presentation" >
+              <DropdownList multi={multi} titleClass={titleClass} subtitleClass={subtitleClass} options={DropdownOptions} onSelect={(value, index) => this.onSelectWrapper(value, onSelectOption, index)} activeListItem={this.state.activeListItem} />
             </ul>
             )}
           </div>
@@ -115,11 +133,11 @@ class Dropdown extends Component {
 }
 const DropdownList = props => (
     !props.multi ?
-    props.options.map((item, index) => <li key={item.title} data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={props.titleClass}>{item.title}</span></li>)
+    props.options.map((item, index) => <li className={props.activeListItem === index ? 'active' : ''} key={item.title} data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={props.titleClass}>{item.title}</span></li>)
     :
-    props.options.map((item, index) => item.subtitle ? <li key={item.title} data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={`${props.titleClass} d-block`}>{item.title}</span><span className={`${props.subtitleClass} d-block`}>{item.subtitle}</span></li>
+    props.options.map((item, index) => item.subtitle ? <li className={props.activeListItem === index ? 'active' : ''} key={item.title} data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={`${props.titleClass} d-block`}>{item.title}</span><span className={`${props.subtitleClass} d-block`}>{item.subtitle}</span></li>
     :
-    <li key={item.title} className="d-flex align-items-center" data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={`${props.titleClass} `}>{item.title}</span></li>)
+    <li className={props.activeListItem === index ? 'd-flex align-items-center active' : 'd-flex align-items-center'} key={item.title} data-value={item.value} role="presentation" onClick={() => props.onSelect(item, index)}><span className={`${props.titleClass} `}>{item.title}</span></li>)
  );
 
 Dropdown.defaultProps = {
