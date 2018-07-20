7f300b97834fe86a6139ad142b7f940edbea1571
James Padinjareveettil Joseph <jamjosep@publicisgroupe.net>
2018-07-20 17:17:32 +0530

    Component Link

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './components/Button';
 import Anchor from './components/Anchor';
+import Link from './components/Link';
 import Drawer from './components/Drawer';
 import ProductCard from './components/ProductCard';
 import PriceDetails from './components/PriceDetails';
@@ -11,4 +12,19 @@ import Checkbox from './components/Checkbox';
 import RadioButton from './components/RadioButton';
 import Modal from './components/Modal';
 import PasswordField from './components/PasswordField';
-export { Button, Anchor, Drawer, ProductCard, PriceDetails, Badge, PopoverStateless, Modal, InputField, Dropdown, Checkbox, RadioButton, PasswordField };
+export {
+  Button,
+  Anchor,
+  Link,
+  Drawer,
+  ProductCard,
+  PriceDetails,
+  Badge,
+  PopoverStateless,
+  Modal,
+  InputField,
+  Dropdown,
+  Checkbox,
+  RadioButton,
+  PasswordField
+};
