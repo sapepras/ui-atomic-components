9715e075db245ae3aa028c66919f80928011bc0e
Amit Raushan <amit.raushan@academy.com>
2018-07-24 19:29:14 +0530

    added tertiary btntype

--- components/Button/SecondaryBtn/index.js
+++ components/Button/SecondaryBtn/index.js
@@ -1,45 +1,37 @@
-
 import { css } from 'react-emotion';
-import {
-    colorPrimary,
-    colorHover,
-    colorWhite,
-    colorLightSteelBlue,
-    colorGrey,
-    getPrimaryColor
-} from '../color';
+import { colorPrimary, colorHover, colorWhite, colorLightSteelBlue, colorGrey, getPrimaryColor } from '../color';
 
 export const secondaryBtnStyles = props => {
-    let borderColor = colorPrimary;
-    const bgBtnColor = '#0000';
-    let textColor = getPrimaryColor(props, colorLightSteelBlue, colorPrimary);
-    const bgDisabledBtnColor = '#0000';
-    let bgHoverColor = 'rgba(0, 0, 0, 0.1)';
-    let hoverBorderColor = colorHover;
-    let hoverTextColor = colorHover;
-    const bgFocusColor = '#0000';
-    let borderFocusColor = colorPrimary;
-    let textActiveColor = colorWhite;
-    let bgActiveColor = colorPrimary;
-    if (props.btnvariant === 'secondary') {
-        borderColor = '#4a4a4a';
-        textColor = getPrimaryColor(props, colorGrey, '#333333');
-        bgHoverColor = '#f6f6f6';
-        hoverBorderColor = '#9b9b9b';
-        hoverTextColor = '#9b9b9b';
-        borderFocusColor = colorGrey;
-        textActiveColor = colorWhite;
-        bgActiveColor = colorGrey;
-    } else if (props.btnvariant === 'tertiary') {
-        borderColor = colorWhite;
-        textColor = colorWhite;
-        hoverBorderColor = colorWhite;
-        hoverTextColor = colorWhite;
-        borderFocusColor = colorWhite;
-        textActiveColor = colorGrey;
-        bgActiveColor = colorWhite;
-    }
-    return css`
+  let borderColor = colorPrimary;
+  const bgBtnColor = '#0000';
+  let textColor = getPrimaryColor(props, colorLightSteelBlue, colorPrimary);
+  const bgDisabledBtnColor = '#0000';
+  let bgHoverColor = 'rgba(0, 0, 0, 0.1)';
+  let hoverBorderColor = colorHover;
+  let hoverTextColor = colorHover;
+  const bgFocusColor = '#0000';
+  let borderFocusColor = colorPrimary;
+  let textActiveColor = colorWhite;
+  let bgActiveColor = colorPrimary;
+  if (props.btnvariant === 'secondary') {
+    borderColor = '#4a4a4a';
+    textColor = getPrimaryColor(props, colorGrey, '#333333');
+    bgHoverColor = '#f6f6f6';
+    hoverBorderColor = '#9b9b9b';
+    hoverTextColor = '#9b9b9b';
+    borderFocusColor = colorGrey;
+    textActiveColor = colorWhite;
+    bgActiveColor = colorGrey;
+  } else if (props.btnvariant === 'tertiary') {
+    borderColor = colorWhite;
+    textColor = colorWhite;
+    hoverBorderColor = colorWhite;
+    hoverTextColor = colorWhite;
+    borderFocusColor = colorWhite;
+    textActiveColor = colorGrey;
+    bgActiveColor = colorWhite;
+  }
+  return css`
     border: 3px solid ${borderColor};
     background-color: ${bgBtnColor};
     color: ${textColor};
@@ -52,29 +44,28 @@ export const secondaryBtnStyles = props => {
     }
 
     &:focus {
-      outline:none;
+      outline: none;
       background-color: ${bgFocusColor};
     }
-    &:focus:before{
-      content: "";
+    &:focus:before {
+      content: '';
       border: 1px dotted ${borderFocusColor};
       border-radius: 35px;
       display: block;
       position: absolute;
       top: -4px;
-      right:-4px;
-      left:-4px;
-      bottom:-4px;
+      right: -4px;
+      left: -4px;
+      bottom: -4px;
     }
     &:active {
-      outline:none;
+      outline: none;
       border-color: ${bgActiveColor};
       color: ${textActiveColor};
       background-color: ${bgActiveColor};
     }
     &:active:before {
-      display:none;
+      display: none;
     }
-`;
+  `;
 };
-
