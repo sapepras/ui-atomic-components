3ccd589525940d43f01a8b428cc93f4565e3434c
Somidi Prashanth Reddy 2 <somreddy@publicisgroupe.net>
2018-08-07 15:09:37 +0530

    code formated

--- components/PriceDetails/lib/css.js
+++ components/PriceDetails/lib/css.js
@@ -13,15 +13,15 @@ export const contentText = css`
 `;
 
 export const toolTipStyles = css`
-    width: 200px;
-    background-color: #ffffff;
-    box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.1), 0 6px 12px 0 rgba(0, 0, 0, 0.04), 0 2px 5px 0 rgba(0, 0, 0, 0.08);
-    font-family: Mallory-Book;
-    font-size: 12px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.33;
-    letter-spacing: normal;
-    color: #333333;
+  width: 200px;
+  background-color: #ffffff;
+  box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.1), 0 6px 12px 0 rgba(0, 0, 0, 0.04), 0 2px 5px 0 rgba(0, 0, 0, 0.08);
+  font-family: Mallory-Book;
+  font-size: 12px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  color: #333333;
 `;
