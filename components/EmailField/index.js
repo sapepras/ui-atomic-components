344fd03637cc306da969279f1045e24ec942cdfd
Vishal Gupta <VGupta66@sapient.com>
2018-09-28 15:23:04 -0500

    Added blur event to handle similar behavior as tab out for domain autosuggest

--- components/EmailField/index.js
+++ components/EmailField/index.js
@@ -39,94 +39,109 @@ const styledInput = props => css`
 `;
 
 export default class EmailField extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.initialValue ? props.initialValue : '',
-            suggestedEmail: ''
-        };
-        this.onChangeInput = this.onChangeInput.bind(this);
-        this.UseSuggestionKeyHandler = this.UseSuggestionKeyHandler.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.initialValue ? props.initialValue : '',
+      suggestedEmail: ''
+    };
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.UseSuggestionKeyHandler = this.UseSuggestionKeyHandler.bind(this);
+  }
 
-    onChangeInput(event, onChange) {
-        const { target } = event;
-        const { value } = target;
-        this.setState({ value });
-        let splitValues;
-        let suggestedEmail;
-        if (value.indexOf('@') !== -1) {
-            splitValues = value.split('@');
-            if (splitValues[1] !== '') {
-                suggestedEmail = `${value}${this.findMatchingEmailDomain(splitValues[1])}`;
-            }
-        }
-        this.setState({ suggestedEmail }, () => onChange(this.state.value));
+  onChangeInput(event, onChange) {
+    const { target } = event;
+    const { value } = target;
+    this.setState({ value });
+    let splitValues;
+    let suggestedEmail;
+    if (value.indexOf('@') !== -1) {
+      splitValues = value.split('@');
+      if (splitValues[1] !== '') {
+        suggestedEmail = `${value}${this.findMatchingEmailDomain(splitValues[1])}`;
+      }
     }
+    this.setState({ suggestedEmail }, () => onChange(this.state.value));
+  }
 
-    UseSuggestionKeyHandler(event, onChange) {
-        if ((event.keyCode === 9 || event.keyCode === 13 || event.keyCode === 39) && (this.state.suggestedEmail && (this.state.suggestedEmail !== this.state.value))) {
-            this.setState({ value: this.state.suggestedEmail }, () => onChange(this.state.value));
-            event.preventDefault();
-        }
+  UseSuggestionKeyHandler(event, onChange) {
+    if (
+      (event.keyCode === 9 || event.keyCode === 13 || event.keyCode === 39 || (event.type && event.type === 'blur')) &&
+      (this.state.suggestedEmail && this.state.suggestedEmail !== this.state.value)
+    ) {
+      this.setState({ value: this.state.suggestedEmail }, () => onChange(this.state.value));
+      event.preventDefault();
     }
+  }
 
-    findMatchingEmailDomain(searchString) {
-        const { domainsList } = this.props;
-        const filteredVal = domainsList.find(item => item.indexOf(searchString) === 0);
-        const filterValLen = searchString.length;
-        return filteredVal ? filteredVal.substr(filterValLen) : '';
-    }
+  findMatchingEmailDomain(searchString) {
+    const { domainsList } = this.props;
+    const filteredVal = domainsList.find(item => item.indexOf(searchString) === 0);
+    const filterValLen = searchString.length;
+    return filteredVal ? filteredVal.substr(filterValLen) : '';
+  }
 
-    render() {
-    const {
-        classname, name, id, disabled, placeholder, onChange, value, auid, initialValue, ...rest
-    } = this.props;
+  render() {
+    const { classname, name, id, disabled, placeholder, onChange, value, auid, initialValue, ...rest } = this.props;
     return (
       <div className={`${styledInput(this.props)}`}>
-        <input data-auid={auid} disabled={disabled} name={name} id={id} type="email" placeholder={placeholder} value={this.state.value} onChange={event => this.onChangeInput(event, onChange)} onKeyDown={event => this.UseSuggestionKeyHandler(event, onChange)} {...rest} />
-        <div data-auid={`${auid}_suggestion`} className="suggestion">{this.state.suggestedEmail}</div>
+        <input
+          data-auid={auid}
+          disabled={disabled}
+          name={name}
+          id={id}
+          type="email"
+          placeholder={placeholder}
+          value={this.state.value}
+          onChange={event => this.onChangeInput(event, onChange)}
+          onBlur={e => this.UseSuggestionKeyHandler(e, onChange)}
+          onKeyDown={event => this.UseSuggestionKeyHandler(event, onChange)}
+          {...rest}
+        />
+        <div data-auid={`${auid}_suggestion`} className="suggestion">
+          {this.state.suggestedEmail}
+        </div>
       </div>
     );
   }
 }
 
 EmailField.defaultProps = {
-    disabled: false,
-    placeholder: '',
-    width: '40rem',
-    height: '2.5rem',
-    borderradius: '4px',
-    borderwidth: '1px',
-    bordercolor: 'rgba(0, 0, 0, 0.2)',
-    fontSize: '1rem',
-    fontWeight: '300',
-    activeborderwidth: '1px',
-    activebordercolor: '#585858',
-    padding: '0.2rem 0.5rem',
-    onChange: () => {},
-    initialValue: ''
+  disabled: false,
+  placeholder: '',
+  width: '40rem',
+  height: '2.5rem',
+  borderradius: '4px',
+  borderwidth: '1px',
+  bordercolor: 'rgba(0, 0, 0, 0.2)',
+  fontSize: '1rem',
+  fontWeight: '300',
+  activeborderwidth: '1px',
+  activebordercolor: '#585858',
+  padding: '0.2rem 0.5rem',
+  onChange: () => {},
+  initialValue: ''
 };
 
 EmailField.propTypes = {
-    domainsList: PropTypes.array.isRequired,
-    classname: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-    disabled: PropTypes.bool,
-    placeholder: PropTypes.string,
-    value: PropTypes.string,
-    width: PropTypes.string,
-    height: PropTypes.string,
-    fontSize: PropTypes.string,
-    bordercolor: PropTypes.string,
-    borderwidth: PropTypes.string,
-    borderradius: PropTypes.string,
-    activebordercolor: PropTypes.string,
-    activeborderwidth: PropTypes.string,
-    fontWeight: PropTypes.string,
-    padding: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    onChange: PropTypes.func,
-    initialValue: PropTypes.string,
-    auid: PropTypes.string
+  domainsList: PropTypes.array.isRequired,
+  classname: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  disabled: PropTypes.bool,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  fontSize: PropTypes.string,
+  bordercolor: PropTypes.string,
+  borderwidth: PropTypes.string,
+  borderradius: PropTypes.string,
+  activebordercolor: PropTypes.string,
+  activeborderwidth: PropTypes.string,
+  fontWeight: PropTypes.string,
+  padding: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  initialValue: PropTypes.string,
+  auid: PropTypes.string
 };
